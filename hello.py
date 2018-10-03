1b63ff2a2f99eeb77c95731d392fdc2b420447c2
windsor929 <windsor929@gmail.com>
2018-10-03 09:32:43 -0400

    This commit makes a flask web app to donwload term form

---
+++ hello.py
@@ -0,0 +1,52 @@
+#this script will lightly test Flask
+#may serve as the base of the web app proof of concept
+
+# -*- coding: utf-8 -*-
+"""
+tiny_example.py
+:copyright: (c) 2015 by C. W.
+:license: GPL v3 or BSD
+"""
+from flask import Flask, request, jsonify
+import flask_excel as excel
+
+app = Flask(__name__)
+
+
+@app.route("/upload", methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        return jsonify({"result": request.get_array(field_name='file')})
+    return '''
+    <!doctype html>
+    <title>Upload an excel file</title>
+    <h1>Excel file upload (csv, tsv, csvz, tsvz only)</h1>
+    <form action="" method=post enctype=multipart/form-data><p>
+    <input type=file name=file><input type=submit value=Upload>
+    </form>
+    '''
+
+
+@app.route("/download", methods=['GET'])
+def download_file():
+    return excel.make_response_from_array([["Elavon HR Termination Spreadsheet","","","","",""],
+    		["INSTRUCTIONS: Please fill out the following columns for every employee you wish to terminate. Leave columns such as 'Immediate Termination' blank if it is not applicable.","","","","",""],
+    		["","","","","",""],["Employee Name","Employee ID","Manager Name","Manager ID","Manager Phone Number","Immediate Termination"]], "csv",
+    		file_name = 'termination_form.xlsx')
+
+
+@app.route("/export", methods=['GET'])
+def export_records():
+    return excel.make_response_from_array([[1, 2], [3, 4]], "csv",
+                                          file_name="export_data")
+
+
+@app.route("/download_file_named_in_unicode", methods=['GET'])
+def download_file_named_in_unicode():
+    return excel.make_response_from_array([[1, 2], [3, 4]], "csv")
+
+
+# insert database related code here
+if __name__ == "__main__":
+    excel.init_excel(app)
+    app.run()
